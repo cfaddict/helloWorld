c645037485cd4aeed46f5ab9138c9a1a5792571f
Dan Vega <danvega@gmail.com>
2017-09-27 17:15:21 -0400

    added another method

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    
+
+    public static void anotherMethod() {
+        System.out.println("My other method.");
+    }
 }
